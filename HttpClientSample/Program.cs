8a7e989311decd8cec178be4b6a95bfab120ee80
e3bauer <e3bauer@uwaterloo.ca>
2018-01-11 15:54:56 -0500

    Add project files.

---
+++ HttpClientSample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace HttpClientSample
+{
+    public class Product
+    {
+        public int id;
+        public string name;
+
+     
+    }
+
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+ 
+            client.BaseAddress = new Uri("https://statsapi.web.nhl.com/api/v1/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                Product product;
+
+                
+                HttpResponseMessage response = await client.GetAsync("teams/15");
+                if (response.IsSuccessStatusCode)
+                {
+                    product = await response.Content.ReadAsAsync<Product>();
+                    Console.WriteLine(product.id);
+                    Console.WriteLine(product.name);
+                }
+
+                
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
