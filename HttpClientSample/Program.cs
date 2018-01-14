4ab7a3f312f91dda94aa0b6a4e7fcc740423a718
e3bauer <e3bauer@uwaterloo.ca>
2018-01-13 23:24:12 -0500

    Extended Team Class

--- HttpClientSample/Program.cs
+++ HttpClientSample/Program.cs
@@ -3,15 +3,78 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace HttpClientSample
 {
     public class Product
     {
-        public int id;
-        public string name;
+        //public int id;
 
-     
+        public string copyright { get; set; }
+        public List<Team> teams { get; set; }
+
+        public Product() { }
+    }
+
+    public class Team
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string link { get; set; }
+        // public Venue ven {get; set;}
+        public string abbreviation { get; set; }
+        public string teamName { get; set; }
+        public string locationName { get; set; }
+        public string firstYearOfPlay { get; set; }
+        // public Division divis {get; set;}
+        // public Conference conf {get;set;}
+        // public Franchise fran {get;set;}
+        public string shortName { get; set; }
+        public string officialSiteUrl { get; set; }
+        public int franchiseId { get; set; }
+        public bool active { get; set; }
+
+        public Team() { }
+
+    }
+    
+    class HockeyAPI
+    {
+        HttpClient client;
+
+        public HockeyAPI()
+        {
+            client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://statsapi.web.nhl.com/api/v1/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<Product> GetTeams()
+        {
+            Product product = null;
+
+            try
+            {
+
+                HttpResponseMessage response = await client.GetAsync("teams");
+                if (response.IsSuccessStatusCode)
+                {
+                    product = await response.Content.ReadAsAsync<Product>();
+                
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return product;
+        }
+
     }
 
     class Program
@@ -20,39 +83,18 @@ namespace HttpClientSample
 
         static void Main()
         {
-            RunAsync().GetAwaiter().GetResult();
-        }
-
-        static async Task RunAsync()
-        {
  
-            client.BaseAddress = new Uri("https://statsapi.web.nhl.com/api/v1/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-
-            try
+            HockeyAPI hapi = new HockeyAPI();
+            Product p;
+            p = hapi.GetTeams().GetAwaiter().GetResult();
+            Console.WriteLine("Copyright notice: " + p.copyright);
+            for (int i = 0; i < p.teams.Count; i++)
             {
-                Product product;
-
-                
-                HttpResponseMessage response = await client.GetAsync("teams/15");
-                if (response.IsSuccessStatusCode)
-                {
-                    product = await response.Content.ReadAsAsync<Product>();
-                    Console.WriteLine(product.id);
-                    Console.WriteLine(product.name);
-                }
-
-                
-
+                Console.WriteLine("ID: " + p.teams[i].id + "\t" + "Team Name: " + p.teams[i].name);
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
             Console.ReadLine();
         }
+
+       
     }
 }
